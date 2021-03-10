e22709d455f457cd21a61f3c51584f7fc12fffec
Ian Neubert <ian@ianneubert.com>
2021-03-09 17:08:16 -0800

    Set a default user agent, and switch to spaces for indentation

--- lib/stock.rb
+++ lib/stock.rb
@@ -2,19 +2,25 @@ require 'nokogiri'
 require 'bigdecimal'
 
 module Stock
-	class Client
-		include HTTParty
+  class Client
+    include HTTParty
 
-		def fund(symbol)
-			puts "Looking for symbol #{symbol} ..."
-			data = self.class.get("https://query1.finance.yahoo.com/v8/finance/chart/#{symbol.upcase}?region=US&lang=en-US&includePrePost=false&interval=1d&range=1mo&corsDomain=finance.yahoo.com&.tsrc=finance").parsed_response
-			
-			price = data['chart']['result'].first['meta']['regularMarketPrice']
+    headers({
+      'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Safari/537.36'
+    })
 
-			date = Time.at(data['chart']['result'].first['meta']['regularMarketTime'])
-			
-			puts "Found price for #{symbol}: #{price}"
-			{price: BigDecimal(price.to_s), date: date}
-		end
-	end
+    def fund(symbol)
+      puts "Looking for symbol #{symbol} ..."
+      data = self.class.get("https://query1.finance.yahoo.com/v8/finance/chart/#{symbol.upcase}?region=US&lang=en-US&includePrePost=false&interval=1d&range=1mo&corsDomain=finance.yahoo.com&.tsrc=finance").parsed_response
+
+      p "here!! #{__FILE__}:#{__LINE__}"
+      p data
+      price = data['chart']['result'].first['meta']['regularMarketPrice']
+
+      date = Time.at(data['chart']['result'].first['meta']['regularMarketTime'])
+
+      puts "Found price for #{symbol}: #{price}"
+      {price: BigDecimal(price.to_s), date: date}
+    end
+  end
 end
